ba7858e9b74bd4c8cec05a417aa142e6592436e1
edikgat <edikgat@gmail.com>
2013-10-08 17:49:54 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleMailSender::Application.config.secret_key_base = 'f58025ae4ced8ae9e2dca120ee64146d1944697cfd89b5270f69b3c9e1cb88889b0dce9746c1d9fb1c5810532e0e950994b17d67839cebc136b40ae316d6d47f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleMailSender::Application.config.session_store :cookie_store, key: '_simple_mail_sender_session'
